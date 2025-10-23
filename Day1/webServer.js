7dcf61c8c67c64f7dc8761fa9473a94fede34cb8
Divyendu Shukla <divyendushukla@Divyendus-MacBook-Air.local>
2025-10-24 01:46:08 +0530

    chore: add crud operation apis

--- Day1/webServer.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-
-http.createServer((req,res)=>{
-    res.writeHead(200,"Divyendu Success",{'Content-Type': 'text/plain'});
-    res.end("Hello world");
-}).listen(8001);
